c4d723040fa6b26e9ec28c356b6ed30f345a3649
mohammad <mohammadmir9891@gmail.com>
2020-11-22 14:36:06 +0330

    Homepage general structure...sidebar colmpleted.

---
+++ src/home.js
@@ -0,0 +1,29 @@
+function onSidebarCollapseClicked(){
+    console.log("sidebar-collapse clicked !!!"); 
+    var classlist = document.getElementById("sidebar").classList;
+    if (classlist.contains("active")){
+        swipeFromRight(classlist, true);
+        classlist.remove("active");
+        console.log('class removed...');
+        return;
+    }
+    swipeFromRight(classlist, false);
+    classlist.add("active");
+    console.log("class added...");
+}
+
+function swipeFromRight(classList, is_reversed){
+    const swipe = "swipeAnimation";
+    const swipe_reversed = "swipeAnimationReversed";
+    if(is_reversed){
+        classList.add(swipe_reversed);
+        setTimeout(() => {
+            classList.remove(swipe_reversed);
+        }, 400);
+        return;
+    }
+    classList.add(swipe);
+    setTimeout(() => {
+        classList.remove(swipe);
+    }, 400);
+}
