e34ad9c784f67544eac406d23e69886b815dbadc
Mahdi Farzadi <mahdifarzadi78@gmail.com>
2020-11-25 14:40:34 +0330

    implement of validation

--- src/sign_in_up.js
+++ src/sign_in_up.js
@@ -20,3 +20,97 @@ function onTabsClicked(event) {
   event.target.classList.add("active");
   event.target.classList.add("bg-light");
 }
+
+function signupValidate(form) {
+  let email = form.email.value;
+  let pass = form.password.value;
+  let pass2 = form.confirm.value;
+  let policy = form.policy.checked;
+  let message;
+  let submit;
+  if (policy) {
+    if (ValidateEmail(email)) {
+      if (pass.trim() != "") {
+        if (pass == pass2) {
+          console.log("ok");
+          return;
+        } else {
+          message = "رمز عبور و تکرار آن باید مطابق هم باشند";
+          form.confirm.focus();
+        }
+      } else {
+        message = "لطفا رمز عبور را وارد کنید";
+        form.password.focus();
+      }
+    } else {
+      message = "لطفا آدرس ایمیل را درست وارد کنید";
+      form.email.focus();
+    }
+  } else {
+    message = "برای ثبت نام لازم است که قوانین و شرایط را پذیرا باشید";
+    form.policy.focus();
+  }
+  submit = document.getElementById("signup_submit");
+  submit.classList.add("shake");
+  setTimeout(() => {
+    submit.classList.remove("shake");
+  }, 500);
+  let alert = document.getElementById("signup_alert");
+  let messageP = document.getElementById("signup_message");
+  messageP.innerText = message;
+  alert.classList.add("show-alert");
+}
+
+function signinValidate(form) {
+  let email = form.email.value;
+  let pass = form.password.value;
+  let message;
+  let submit;
+  if (ValidateEmail(email)) {
+    if (pass.trim() != "") {
+      console.log("ok");
+      return;
+    } else {
+      message = "لطفا رمز عبور را وارد کنید";
+      form.password.focus();
+    }
+  } else {
+    message = "لطفا آدرس ایمیل را درست وارد کنید";
+    form.email.focus();
+  }
+  submit = document.getElementById("signin_submit");
+  submit.classList.add("shake");
+  setTimeout(() => {
+    submit.classList.remove("shake");
+  }, 500);
+  let alert = document.getElementById("signin_alert");
+  let messageP = document.getElementById("signin_message");
+  console.log(alert.classList.remove("show-alert"));
+  messageP.innerText = message;
+  alert.classList.add("show-alert");
+}
+
+function ValidateEmail(email) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (email.match(mailformat)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function onCloseClicked() {
+  let alerts = document.getElementsByClassName("alert-div");
+  for (alert of alerts) {
+    alert.classList.add("close-alert");
+    setTimeout(
+      a => {
+        a.classList.remove("show-alert");
+        a.classList.remove("close-alert");
+        console.log(a);
+      },
+      450,
+      alert
+    );
+  }
+}
